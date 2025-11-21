d80c042c576887747e6e2f78e5e527a9aedd5631
Izzy Schrack <superizzy@outlook.com>
2025-11-20 20:44:42 -0800

    updated stuff that didn't get merged
    Co-Authored-By: rmoren15@ucsc.edu <187337200+romeomore@users.noreply.github.com>

--- main.lua
+++ main.lua
@@ -1,28 +1,3 @@
---[[local currentScene
-
-function love.load()
-  currentScene = require("scenes.menu") -- Load the initial scene
-  currentScene.load()
-end
-
-function love.update(dt)
-  if currentScene and currentScene.update then
-      currentScene.update(dt)
-  end
-end
-
-function love.draw()
-  if currentScene and currentScene.draw then
-      currentScene.draw()
-  end
-end
-
-function changeScene(newSceneName)
-  self.setScene("menu")
-  currentScene.load()
-end--]]
-
--- main.lua (Recommended Automatic Loading Setup)
 local SceneryInit = require("scenery")
 
 -- "menu" is the scene key (a string)
